b1fba06467de2727e240c61a64b754104f645950
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-12-17 16:55:03 -0500

    just starting over without the oauth stuff

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,8 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'default')]
     public function index(): Response
     {
-        return $this->redirectToRoute('admin');
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
     }
 }
