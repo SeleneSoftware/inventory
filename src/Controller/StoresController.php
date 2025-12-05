042ebcb3d16679bc23f0fe3ce76dd48034219409
Jason Marshall <jasonmarshallselene@gmail.com>
2025-12-04 23:28:35 -0500

    added some security stuff

--- src/Controller/StoresController.php
+++ src/Controller/StoresController.php
@@ -9,7 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 final class StoresController extends AbstractController
 {
     #[Route('/stores', name: 'app_stores')]
