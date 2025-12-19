5a810b21cdac5cd9b9f683b3d791662a90938696
Jason Marshall <jasonmarshallselene@gmail.com>
2025-12-19 10:36:34 -0500

    form for making new categories is available

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -8,25 +8,39 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfonycasts\DynamicForms\DependentField;
+use Doctrine\ORM\EntityRepository;
+use Symfonycasts\DynamicForms\DynamicFormBuilder;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder = new DynamicFormBuilder($builder);
+
         $builder
             ->add('name')
-            ->add('parent', EntityType::class, [
-                'class' => Category::class,
-                'choice_label' => 'name',
-                'placeholder' => 'Choose One',
-                'required'=>false,
-            ])
             ->add('store', EntityType::class, [
                 'class' => Store::class,
                 'choice_label' => 'name',
                 'placeholder' => 'Choose One',
             ])
-        ;
+            ->addDependent('parent', 'store', function (dependentField $field, ?Store $store) {
+                $field->add(EntityType::class, [
+                    'class' => Category::class,
+                    'query_builder' => function (EntityRepository $er) use ($store) {
+                        return $er->createQueryBuilder('c')
+                                  ->where('c.store = :store')
+                                  ->setParameter('store', $store);
+                    },
+                    'choice_label' => 'name',
+                    'placeholder' => 'Choose One',
+                    'required'=>false,
+                ])
+                ;
+
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
---
+++ src/Twig/Components/CategoryChooser.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Twig\Components;
+
+use App\Form\CategoryType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+class CategoryChooser extends AbstractController
+{
+    use ComponentWithFormTrait;
+    use DefaultActionTrait;
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createForm(CategoryType::class);
+    }
+}
+
+?>
