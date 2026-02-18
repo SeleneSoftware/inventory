40c89a930f133f9e87f056f0598fa02d8f7e6dfe
Jason Marshall <jasonmarshallselene@gmail.com>
2026-02-18 10:59:38 -0500

    fixes for the docker stuff

--- docker-files/certbot.sh
+++ docker-files/certbot.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+CMD=docker
+if command -v podman >/dev/null 2>&1
+then
+	CMD=podman
+fi
 
-sudo podman compose run --rm certbot certonly --manual --preferred-challenges dns -d $1
+
+sudo $CMD compose run --rm certbot certonly --manual --preferred-challenges dns -d $1
 
