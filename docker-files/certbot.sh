b1d715a8b23e5b788cf54ca8eac902c227cdebff
Jason Marshall <jasonmarshallselene@gmail.com>
2026-02-23 12:30:25 -0500

    fixing some docker stuff

--- docker-files/certbot.sh
+++ docker-files/certbot.sh
@@ -6,5 +6,5 @@ then
 fi
 
 
-sudo $CMD compose run --rm certbot certonly --manual --preferred-challenges dns -d $1
+$CMD compose run --rm certbot certonly --manual --preferred-challenges dns -d $1
 
