d2c3f2caefdd7bcdee9d7d228422e25f83ccc4d5
Jason Marshall <jasonmarshallselene@gmail.com>
2026-04-21 08:30:04 -0400

    working on the manual rest controllers, maybe not for me

--- src/Controller/ApiStoresController.php
+++ src/Controller/ApiStoresController.php
@@ -3,14 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\StoreRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ApiController extends AbstractController
+final class ApiStoresController extends AbstractController
 {
     #[Route('/api/stores', name: 'app_api_stores')]
+    public function index(StoreRepository $repo, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if ($request->isMethod('POST')) {
+            return $this->storesUpdate($repo, $request, $entityManager);
+        }
+
+        return $this->stores($repo, $request);
+    }
+
     public function stores(StoreRepository $repo, Request $request): Response
     {
         foreach ($request->query as $field => $query) {
@@ -24,6 +34,10 @@ final class ApiController extends AbstractController
                 default:
                     $results = $repo->findAll();
             }
+        }
+        // If this looks redundant, it's because it has to be.
+        if (!isset($results)) {
+            $results = $repo->findAll();
         }
 
         $array = [];
@@ -45,6 +59,31 @@ final class ApiController extends AbstractController
                 'categories' => $categories,
             ];
         }
+
+        return $this->json($array);
+    }
+
+    public function storesUpdate(StoreRepository $repo, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$request->query->get('id')) {
+            return $this->json(['error' => 'Nothing']);
+        }
+
+        $store = $repo->find($request->query->get('id'));
+        $categories = [];
+        foreach ($store->getCategories() as $cat) {
+            $categories[] = [
+                'name' => $cat->getName(),
+                'apiLink' => 'TBA: '.$cat->getName(),
+            ];
+        }
+
+        $array[] = [
+            'id' => $store->getId(),
+            'name' => $store->getName(),
+            'link' => $store->getLink(),
+            'categories' => $categories,
+        ];
 
         return $this->json($array);
     }
